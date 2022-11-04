eed7eb9f9b91b6fb2261fe2527094c6ba956de8a
Unnati2310 <101443031+Unnati2310@users.noreply.github.com>
2022-11-04 15:39:40 +0530

    Update Q1.py

--- python_exam/Q1.py
+++ python_exam/Q1.py
@@ -5,8 +5,13 @@ Note: You may use may define any additional class, functions if necessary.
 However, DO NOT CHANGE THE TEMPLATE CHANGE THE TEMPLATE OF THE FUNCTIONS PROVIDED.
 """
 
-
+import os
 def Dij_generator():
+    File1 = open("Chicago-Sketch/ChicagoSketch_net.tntp", "r")
+    data= my_file.read()
+    data_into_list = data.replace('\n', ' ').split(".")
+    print(data_into_list)
+    File1.close()
     """
     Reads the ChicagoSketch_net.tntp and convert it into suitable python object on which you will implement shortest-path algorithms.
 
@@ -16,8 +21,27 @@ def Dij_generator():
     graph_object = None
     try:
         # Enter your code here
+        n=933
+        m=2950
+        for i in range(n)
+        distance[i+1] = INF
+        distance[x] = 0
+        q.push({0,x})
+        while (!q.empty())
+        int a = q.top().second 
+        q.pop()
+        if (processed[a])
+        continue
+        processed[a] = true
+
+        for (auto u : adj[a])
+        int b = u.first, w = u.second
+        if (distance[a]+w < distance[b])
+        distance[b] = distance[a]+w
+        q.push({-distance[b],b});
+
         return graph_object
-    except:
+    except: 
         return graph_object
 
 
